70901f5e1f131a4c651bfd1610c52f65652cdd89
renatho <renatho.20gomes@gmail.com>
2024-01-15 12:19:57 -0300

    user req1

--- src/migrations/20231207124456-users.js
+++ src/migrations/20231207124456-users.js
@@ -1,14 +1,32 @@
 'use strict';
 
+const { allow } = require('joi');
+
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+    await queryInterface.createTable('users', {
+      id: {
+        primaryKey: true,
+        allowNull: false,
+        autoIncrement: true,
+        type: Sequelize.INTEGER
+
+      },
+      display_name: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      email: {
+        type: Sequelize.STRING
+      },
+      password: {
+        type: Sequelize.STRING
+      },
+      image: {
+        type: Sequelize.STRING
+      },
+    });
   },
 
   async down (queryInterface, Sequelize) {
