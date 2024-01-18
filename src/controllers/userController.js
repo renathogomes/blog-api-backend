32acc0855e0d64474e212e557d19fc2eb398a93c
renatho <renatho.20gomes@gmail.com>
2024-01-18 11:43:42 -0300

    req 4 em andamento

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -14,15 +14,21 @@ const userNewUserToken = async (req, res) =>  {
     // Configurar informações do token
     const tokenData = { data: { emailUser } };
 
+    //Gerar token config
+    const tokenConfig = {
+      expiresIn: '7d',
+      algorithm: 'HS256',
+    };
+
     // Gerar token JWT
-    const token = jwt.sign(tokenData, secret, { expiresIn: '7d', algorithm: 'HS256' });
+    const token = jwt.sign(tokenData, secret, tokenConfig);
 
     // Retornar resposta com status 201 e token gerado
     res.status(201).json({ token });
 
   } catch (error) {
     console.error('Error during user creation and token generation:', error);
-    res.status(500).json({ error: 'Internal Error' });
+    res.status(500).json({ message: 'Internal Error' });
   }
 }
 
--- src/middlewares/userMiddleware.js
+++ src/middlewares/userMiddleware.js
@@ -4,32 +4,31 @@ const userMiddleware = async (req, res, next) => {
   try {
     const { displayName, password, email } = req.body;
 
-    if (displayName.length < 8) {
-      return res.status(400).json({ error: '"displayName" length must be at least 8 characters long' });
+    if (displayName.length < 8 || !displayName) {
+      return res.status(400).json({ message: '"displayName" length must be at least 8 characters long' });
     }
 
     // Validar o formato do email usando uma expressão regular
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     if (!emailRegex.test(email)) {
-      return res.status(400).json({ error: '"email" must be a valid email' });
+      return res.status(400).json({ message: '"email" must be a valid email' });
     }
 
     // Verificar o comprimento do password
     if (password.length < 6) {
-      return res.status(400).json({ error: '"password\" length must be at least 6 characters long' });
+      return res.status(400).json({ message: '"password\" length must be at least 6 characters long' });
     }
 
     // Verificar se o usuário já está registrado com base no email
     const user = await loginService.findEmail(email);
-
     if (user) {
-      return res.status(409).json({ error: 'User already registered' });
+      return res.status(409).json({ message: 'User already registered' });
     }
 
     next();
   } catch (err) {
     console.error('Error when verifying user by email:', err);
-    return res.status(500).json({ error: 'Internal server error.' });
+    return res.status(500).json({ message: 'Internal server error.' });
   }
 };
 
